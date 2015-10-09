0b063163b9a87e7f558c4775bca7fbf5b6e2b4b1
adamhurwitz <adamscotthurwitz@gmail.com>
2015-10-08 23:23:08 -0700

    Shared Preference Test

--- app/src/main/java/com/example/adamhurwitz/hackingenvironment/MainActivity.java
+++ app/src/main/java/com/example/adamhurwitz/hackingenvironment/MainActivity.java
@@ -1,27 +1,49 @@
 package com.example.adamhurwitz.hackingenvironment;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.android.hackingenv";
 
-    /** Called when the user clicks the Send button */
+    /**
+     * Called when the user clicks the Send button
+     */
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this,
                 com.example.adamhurwitz.hackingenvironment.DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        /*EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_MESSAGE, message);*/
         startActivity(intent);
     }
+
+// SharedPref - In MainActivity vs. in MainActivityFragment.java
+
+   /* public void sendToSharedPreferences(View view) {
+        EditText editTextField1 = (EditText) this.findViewById(R.id.edit_message_one);
+        String firstPref = editTextField1.getText().toString();
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.preference_test), firstPref);
+        editor.commit();
+    }
+
+    public void getFromSharedPreferences(View view) {
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = getResources().getString(R.string.saved_default_value);
+        String preference = sharedPref.getString(getString(R.string.preference_test), defaultValue);
+
+        TextView sharedPrefOutput = (TextView) this.findViewById(R.id.shared_pref_output);
+        sharedPrefOutput.setText(preference);
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/adamhurwitz/hackingenvironment/MainActivityFragment.java
+++ app/src/main/java/com/example/adamhurwitz/hackingenvironment/MainActivityFragment.java
@@ -1,10 +1,15 @@
 package com.example.adamhurwitz.hackingenvironment;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,46 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View inflatedView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button b1 = (Button) inflatedView.findViewById(R.id.writeToSharedPref);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendToSharedPreferences(v);
+            }
+        });
+
+        Button b2 = (Button) inflatedView.findViewById(R.id.getFromSharePref);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFromSharedPreferences(v);
+            }
+        });
+
+        return inflatedView;
+    }
+
+    // Write to Shared Preferences
+    public void sendToSharedPreferences(View view) {
+        EditText userPref = (EditText) getActivity().findViewById(R.id.edit_message_one);
+        String firstPref = userPref.getText().toString();
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.preference_test), firstPref);
+        editor.commit();
+    }
+
+    // Read from Shared Preferences
+    public void getFromSharedPreferences(View view) {
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String defaultValue = getResources().getString(R.string.saved_default_value);
+        String preference = sharedPref.getString(getString(R.string.preference_test), defaultValue);
+
+        TextView sharedPrefOutput = (TextView) getActivity().findViewById(R.id.shared_pref_output);
+        sharedPrefOutput.setText(preference);
     }
 }
