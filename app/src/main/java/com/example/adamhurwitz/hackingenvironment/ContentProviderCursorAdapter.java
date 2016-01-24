663f9e82c98f9a5d01a4be20cc270ed92ac02e62
adamhurwitz <adamscotthurwitz@gmail.com>
2016-01-23 22:50:23 -0800

    Favorite btn works on GridView

--- app/src/main/java/com/example/adamhurwitz/hackingenvironment/ContentProviderCursorAdapter.java
+++ app/src/main/java/com/example/adamhurwitz/hackingenvironment/ContentProviderCursorAdapter.java
@@ -3,14 +3,12 @@ package com.example.adamhurwitz.hackingenvironment;
 import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.example.adamhurwitz.hackingenvironment.data.CursorContract;
-import com.example.adamhurwitz.hackingenvironment.data.CursorDbHelper;
+import com.example.adamhurwitz.hackingenvironment.data.ContentProviderContract;
 import com.squareup.picasso.Picasso;
 
 
@@ -23,7 +21,7 @@ public class ContentProviderCursorAdapter extends android.widget.CursorAdapter {
      *
      * @param context The context in which this adapter is called.
      * @param cursor  Cursor from which to get the data
-     * @param flags Determine behavior of adapter
+     * @param flags   Determine behavior of adapter
      */
     // creates constructor to create StaticArrayAdapter object
     public ContentProviderCursorAdapter(Context context, Cursor cursor, int flags) {
@@ -58,27 +56,37 @@ public class ContentProviderCursorAdapter extends android.widget.CursorAdapter {
         // as ImageView
         holder.gridItem = (ImageView) view.findViewById(R.id.grid_contentprovideritem_imageview);
         view.setTag(holder);
+        holder.favoriteGridBtn = (ImageView) view.findViewById(R.id.gridItem_favorite_button);
 
         return view;
     }
 
     public void bindView(View view, Context context, Cursor cursor) {
         // Access database
-        CursorDbHelper mDbHelper = new CursorDbHelper(context);
+        //CursorDbHelper mDbHelper = new CursorDbHelper(context);
         // Gets the data repository in read mode
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         String imageURL = cursor.getString(
-                cursor.getColumnIndexOrThrow(CursorContract.ProductData.COLUMN_NAME_IMAGEURL));
+                cursor.getColumnIndexOrThrow(ContentProviderContract.ContentProviderProductData
+                        .COLUMN_NAME_IMAGEURL));
 
         // Holder for a view
         ViewHolder holder = (ViewHolder) view.getTag();
         Picasso.with(context).load(imageURL).noFade()
                 .into(holder.gridItem);
+
+        String favorite = cursor.getString(cursor.getColumnIndexOrThrow(ContentProviderContract
+                .ContentProviderProductData.COLUMN_NAME_FAVORITE));
+        if (favorite.equals("1")) {
+            holder.favoriteGridBtn.setImageResource(R.drawable.star_default_18dp);
+        }
+        else{holder.favoriteGridBtn.setImageResource(R.drawable.star_pressed_18dp);}
     }
 
     class ViewHolder {
         // declare your views here
         ImageView gridItem;
+        ImageView favoriteGridBtn;
     }
 }
