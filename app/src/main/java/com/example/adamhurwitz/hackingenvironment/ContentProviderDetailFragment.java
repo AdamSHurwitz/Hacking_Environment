587e473a342364bafdee267406a8eed60a5542d6
adamhurwitz <adamscotthurwitz@gmail.com>
2016-01-23 23:58:06 -0800

    Updated TODOs

--- app/src/main/java/com/example/adamhurwitz/hackingenvironment/ContentProviderDetailFragment.java
+++ app/src/main/java/com/example/adamhurwitz/hackingenvironment/ContentProviderDetailFragment.java
@@ -98,7 +98,7 @@ public class ContentProviderDetailFragment extends Fragment {
                 favVal = cursor.getString(favColIndex);
             }
 
-            //Logic to handle Favoirte Button status on load
+            //Logic to handle Favorite Button status on load
             if (favVal.equals("1")) {
                 favoriteButton.setImageResource(R.drawable.star_default_18dp);
             } else {
